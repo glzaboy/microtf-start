b1526bfc1695ab455adce144bfceec06a0001575
glzaboy <glzaboy@163.com>
2023-03-12 16:13:13 +0800

    整合mui

---
+++ config/theme.ts
@@ -0,0 +1,29 @@
+import { Roboto } from 'next/font/google'
+import { createTheme } from '@mui/material/styles'
+import { red } from '@mui/material/colors'
+
+export const roboto2 = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+})
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#786800',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  typography: {
+    fontFamily: roboto2.style.fontFamily,
+  },
+})
+
+export default theme
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,27 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { Container, Box, Typography } from '@mui/material'
 
 const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>第一个页面</title>
       </Head>
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Material UI - Next.js example
+          </Typography>
+          {/* <Link href="/about" color="secondary">
+            Go to the about page
+          </Link> */}
+          {/* <ProTip /> */}
+          {/* <Copyright /> */}
+        </Box>
+      </Container>
     </>
   )
 }
